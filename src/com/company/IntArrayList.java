79fd34546441e97134b705f18efa8098eaa06914
IvanFromOdesa <krylosov382@gmail.com>
2022-07-17 23:36:57 +0300

    Refactoring

--- src/com/company/IntArrayList.java
+++ src/com/company/IntArrayList.java
@@ -3,59 +3,52 @@ package com.company;
 import java.util.Arrays;
 
 public class IntArrayList implements IntList {
-    private int arr[]=new int[10];
+    private int[] arr =new int[10];
     private int size=0;
 
     @Override
-    public void add(int element){
+    public void add(int element) {
         if(size>=arr.length){
             int tempSize=arr.length*10;
             int[] tempArr = new int[tempSize];
-            for (int i = 0; i < size; i++) {
-                tempArr[i] = arr[i];
-            }
+            System.arraycopy(arr, 0, tempArr, 0, size);
             arr = tempArr;
         }
         arr[size]=element;
         size++;
     }
     @Override
-    public boolean add(int index, int element){
+    public boolean add(int index, int element) {
         /*new array for the temporary storage of elements
         with index starting from the indicated one
          */
-        int newArr[]= new int[arr.length];
+        int[] newArr = new int[arr.length];
         int y=0; //iterator for transferring values from arr to newArr
         int j=0; // iterator for transferring values from newArr back to the arr
 
-        if(size>=arr.length){
+        if (size>=arr.length) {
             int tempSize=arr.length*10;
             int[] tempArr = new int[tempSize];
-            for (int i = 0; i < size; i++) {
-                tempArr[i] = arr[i];
-            }
+            System.arraycopy(arr, 0, tempArr, 0, size);
             arr = tempArr;
         }
 
         /*Transferring values from arr to newArr*/
-        for(int i=index; i<size;i++){
+        for (int i=index; i<size;i++) {
             newArr[y]=arr[i];
             y++;
         }
         size++;
 
         /*Pasting an element with the indicated index.Transferring values from newArr back to arr*/
-        for(int i=index; i<=size+1;i++){
+        for (int i=index; i<=size+1;i++) {
             arr[index]=element;
             if(i!=index){
                 arr[i]=newArr[j];
                 j++;
             }
         }
-        if(index>arr.length||index<0||index>size){
-            return false;
-        }
-        return true;
+        return index <= arr.length && index >= 0 && index <= size;
     }
 
     @Override
@@ -69,17 +62,14 @@ public class IntArrayList implements IntList {
     };
 
     @Override
-    public boolean isEmpty(){
-        if(size==0){
-            return true;
-        }
-        return false;
+    public boolean isEmpty() {
+        return size == 0;
     };
 
     @Override
-    public boolean remove(int index){
-        int newArr[]=new int[arr.length-1];
-        if(0<=index||index<size){
+    public boolean remove(int index) {
+        int[] newArr =new int[arr.length-1];
+        if(0<=index||index<size) {
             System.arraycopy(arr,0, newArr, 0, index);
             System.arraycopy(arr, index+1, newArr, index, size-index-1);
             size--;
@@ -90,8 +80,8 @@ public class IntArrayList implements IntList {
     };
 
     @Override
-    public boolean removeByValue(int value){
-        int newArr[]=new int[arr.length-1]; //at least 1 element may be deleted
+    public boolean removeByValue(int value) {
+        int[] newArr =new int[arr.length-1]; //at least 1 element may be deleted
         int count=0; // to count how many elements with the same value were deleted
         boolean flag=false;
         for(int i=0; i<size; i++){
@@ -108,7 +98,7 @@ public class IntArrayList implements IntList {
     };
 
     @Override
-    public boolean set(int index, int element){
+    public boolean set(int index, int element) {
         if(0<=index||index<size){
             arr[index]=element;
             return true;
@@ -122,7 +112,7 @@ public class IntArrayList implements IntList {
     };
 
     @Override
-    public  IntArrayList subList(int fromIndex, int toIndex){
+    public  IntArrayList subList(int fromIndex, int toIndex) {
         IntArrayList newArr=new IntArrayList();
         if(fromIndex<toIndex||0<=fromIndex||toIndex<size){
             for(int i=fromIndex;i<=toIndex-fromIndex;i++){
@@ -134,11 +124,9 @@ public class IntArrayList implements IntList {
     };
 
     @Override
-    public int[] toArray(){
+    public int[] toArray() {
         int[] result = new int[size];
-        for(int i=0; i<size; i++) {
-            result[i] = arr[i];
-        }
+        System.arraycopy(arr, 0, result, 0, size);
         return result;
     }
 
